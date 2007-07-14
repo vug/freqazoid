c5686770534882efae84d4c7e3ce8549ea488df2
ugurguney <ugurguney@86e63ff1-882e-0410-812e-1d9ab476d632>
2007-07-14 14:06:24 +0000

    Initial import.

---
+++ trunk/freqazoid/src/realtimesound/Main.java
@@ -0,0 +1,30 @@
+/*
+ * Main.java
+ *
+ * Created on March 24, 2007, 2:03 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package realtimesound;
+
+/**
+ *
+ * @author HAL
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {        
+        ResourceManager rm = new ResourceManager();     
+        /*byte b1 = 0;
+        byte b2 = 2;
+        
+        int x;
+        x = (int)b1 + (((int)b2)<<8);
+        System.out.println(x);*/
+    }
+    
+}
---
+++ trunk/freqazoid/src/realtimesound/ResourceManager.java
@@ -0,0 +1,72 @@
+/*
+ * ResourceManager.java
+ *
+ * Created on March 25, 2007, 4:24 AM
+ */
+
+package realtimesound;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author HAL
+ */
+public class ResourceManager implements ActionListener {
+    
+    private AudioEngine audioEngine;
+    private Thread audioThread;
+    private JFrame window = new JFrame("Audio Engine");
+    private JButton buttonPause = new JButton("pause");
+    private JButton buttonStop = new JButton("stop");
+    private Canvas canvas;
+    
+    /** Creates a new instance of ResourceManager */
+    public ResourceManager() {
+        System.out.println("Opening audio system...");
+        audioEngine = new AudioEngine(this);
+        System.out.println("Audio system opened.");
+        audioThread = new Thread(audioEngine);        
+        System.out.println("starting the audio engine...");        
+        audioThread.start();
+        
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(500,200);
+        window.setLocation(100,100);
+        window.setLayout(new BorderLayout());        
+        
+        buttonPause.addActionListener(this);
+        window.add(buttonPause, BorderLayout.PAGE_START);
+        
+        buttonStop.addActionListener(this);        
+        window.add(buttonStop, BorderLayout.PAGE_END);
+        
+        canvas = new Canvas();
+        window.add(canvas, BorderLayout.CENTER);
+        
+        window.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==buttonPause) {
+            audioEngine.pauseEngine();
+        }
+        if(e.getSource()==buttonStop) {
+            System.out.println("stopping");
+            audioEngine.stopEngine();
+        }
+    }
+    
+    public Canvas getCanvas() {
+        return this.canvas;
+    }
+    
+}
