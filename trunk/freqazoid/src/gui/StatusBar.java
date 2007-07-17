339f20a37119d639c49bea85c5eeea13005515f9
ugurguney <ugurguney@86e63ff1-882e-0410-812e-1d9ab476d632>
2007-07-17 11:39:12 +0000

    Initial import.

---
+++ trunk/freqazoid/src/gui/StatusBar.java
@@ -0,0 +1,32 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import realtimesound.ResourceManager;
+
+public class StatusBar extends JPanel {
+	
+	private JLabel labelWidthDuration;
+	private ResourceManager rm;
+	
+	public StatusBar(ResourceManager rm) {
+		super(new FlowLayout(FlowLayout.RIGHT));
+		
+		this.rm = rm;
+		String duration = Double.toString((double)rm.getCanvas().getNPoints()/44.1).substring(0,3);
+		labelWidthDuration = new JLabel(duration+" msec");	
+		
+		//labelWidthDuration.setPreferredSize(new Dimension(80,10));
+		labelWidthDuration.setBackground(Color.MAGENTA);
+		//this.setMaximumSize(new Dimension(1000,20));
+		//this.setPreferredSize(new Dimension(10,20));
+
+		this.add(labelWidthDuration);
+	}
+
+}
