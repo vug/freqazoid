bb2d9e399ac4e3b98f5971d7c97fe9d1d0025122
ugurguney <ugurguney@86e63ff1-882e-0410-812e-1d9ab476d632>
2008-01-24 07:14:21 +0000

    threshold is made a function of frequency. parabolic fit for successive 3 magnitude points is triad, but not worked well and commented out.

--- freqazoid/src/math/PeakDetector.java
+++ freqazoid/src/math/PeakDetector.java
@@ -7,24 +7,44 @@ public class PeakDetector {
 	 * tum pointlerin uzerinden ucer ucer gecerek peak'lerin tepelerini tespit et.
 	 */
 	
+	private static double peakThreshold = 0.5;
+	
 	public static final Peak[] detectSpectralPeaks(double[] point, double threshold) {
 		Vector<Peak> peaks = new Vector<Peak>();
 		int N = point.length;
 		
+		double[] pointDB = new double[N];
+		
+		
 		for (int i = 0; i < point.length; i++) {
-			point[i] = Tools.lin2dB(point[i]);
+			pointDB[i] = Tools.lin2dB(point[i]);
 		}
 		
 		
 		for(int i=1; i<N-1; i++) {
-			if( point[i]>point[i-1] && point[i+1]<point[i]&& point[i]>threshold ){
-				peaks.add(new Peak( (i*22050.0)/point.length, point[i]));
+			double freq = (i*22050.0)/point.length;
+			if( pointDB[i]>pointDB[i-1] && pointDB[i+1]<pointDB[i]&& pointDB[i]>threshold(freq) ){
+				peaks.add(new Peak( freq, point[i]));
+				
+//				double p = 0.5*Math.log(point[i-1]*point[i+1])/Math.log(point[i-1]*point[i+1]/(point[i]*point[i]));
+//				peaks.add(new Peak( (i+p)*22050.0/point.length ,point[i]));
 			}
 		}
-		
-		//System.out.println(peaks.size());
+
 		peaks.trimToSize();
 		Peak[] ret = new Peak[peaks.size()];
 		return peaks.toArray(ret);
+	}
+	
+	public static double getPeakThreshold() {
+		return peakThreshold;
+	}
+
+	public static void setPeakThreshold(double threshold) {
+		peakThreshold = threshold;
+	}
+	
+	public static final double threshold(double frequency) {
+		return peakThreshold;
 	}
 }
