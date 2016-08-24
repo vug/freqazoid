dd6fdca06a192858a66edfdaff51bab02348588f
Ugur Guney <ugurguney@gmail.com>
2016-08-24 01:37:20 -0400

    use let's in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-mymodule = require('./mymodule');
+let mymodule = require('./mymodule');
+let myvar = mymodule.myvariable;
+let myfunc = mymodule.myfunction;
 
 console.log('Hello, from JS!');
-console.log('This is a variable:', mymodule.myvariable);
-console.log('This is a function result:', mymodule.myfunction(3, 5));
+console.log('This is a variable:', myvar);
+console.log('This is a function result:', myfunc(3, 5));
