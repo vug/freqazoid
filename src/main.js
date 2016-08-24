c85bf1152c74ba468a3e0097605ba2e67f07b318
Ugur Guney <ugurguney@gmail.com>
2016-08-24 00:11:32 -0400

    use example module
    print module related stuff to console

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
+mymodule = require('./mymodule');
+
 console.log('Hello, from JS!');
+console.log('This is a variable:', mymodule.myvariable);
+console.log('This is a function result:', mymodule.myfunction(3, 5));
