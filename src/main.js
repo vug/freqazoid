67f46c3786873773e9f4230a18c4e4849954a969
Ugur Guney <ugurguney@gmail.com>
2016-08-24 21:43:01 -0400

    remove test console.log

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ let mymodule = require('./mymodule');
 let myvar = mymodule.myvariable;
 let myfunc = mymodule.myfunction;
 
-console.log('Hello, from JS!');
 console.log('This is a variable:', myvar);
 console.log('This is a function result:', myfunc(3, 5));
 
