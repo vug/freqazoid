519b6bc1ba8e8f155a4129b659f96128b5bada40
Ugur Guney <ugurguney@gmail.com>
2016-08-24 21:41:17 -0400

    function to have single way of interaction with vendor APIs

--- src/main.js
+++ src/main.js
@@ -5,3 +5,15 @@ let myfunc = mymodule.myfunction;
 console.log('Hello, from JS!');
 console.log('This is a variable:', myvar);
 console.log('This is a function result:', myfunc(3, 5));
+
+/**
+ Abstract away from browser vendor prefixes for various APIs that are needed by the app
+*/
+let abstractAwayVendorPrefixes = function() {
+    navigator.getUserMedia = (
+        navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia
+    );
+
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+};
